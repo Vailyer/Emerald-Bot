74d0d571fecc521950a7117144869cea2c684e0e
Vailyer <Montydavidhollings@gmail.com>
2017-06-26 01:52:44 +0100

    Added test, ping, avatar and a feature to auto send a message when a new user joins

--- Emerald.js
+++ Emerald.js
@@ -11,10 +11,41 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to the guilds default channel (usually #general), mentioning the member
+  member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
+
+  // If you want to send the message to a designated channel on a server instead
+  // you can do the following:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
+//!ping will reply pong 
 client.on('message', message => {
-  if (message.content === '!!test') {
-    message.reply('Emerald Is Loaded!');
+  if (message.content === '!ping') {
+    message.reply('pong!');
   }
 });
+
+//!avatar will reply a link to the users avatar
+client.on('message', message => {
+  if (message.content === '!avatar') {
+    message.reply(message.author.avatarURL);
+  }
+});
+
+//!test will reply if the bot is working
+client.on('message', message => {
+  if (message.content === '!test') {
+    message.reply('Emerald Is Working!');
+  }
+});
+
 
 client.login(token);
