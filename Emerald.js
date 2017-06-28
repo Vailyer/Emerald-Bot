d695913b194a77f52ec673cf992fd08494ae8e71
Vailyer <montydavidhollings@gmail.com>
2017-06-28 23:09:20 +0100

    Update (read description)
    Added Eval, changed tokens, added config.json, added user join log

--- Emerald.js
+++ Emerald.js
@@ -4,48 +4,84 @@ const Discord = require('discord.js');
 //Creating new Discord client instance.
 const client = new Discord.Client();
 
-// Creating constant for bot token
-const token = 'MzI4MjUzOTMzODYyNTE4ODAw.DDBN2Q.02r6cwlWuoalNSKV7RzU6TgGHDQ';
+//Import the data from the config.json file. (github hidden you need to make your own)
+const config = require("./config.json");
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
+//Both bits below Make it so when using the EVAL function / command a user cant mention another user (for safety) by adding a micro character
+//In between making it cancel.
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+
+//The actual eval command / code
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+
+  if (message.content.startsWith("/eval")) {
+    if(message.author.id !== config.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+});
+
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-  // Send the message to the guilds default channel (usually #general), mentioning the member
-  member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
-
-  // If you want to send the message to a designated channel on a server instead
-  // you can do the following:
-  const channel = member.guild.channels.find('name', 'member-log');
+  const channel = member.guild.channels.find('name', 'general');
+  channel.send("Welcome to the server, ${member}");
+  const logchannel = member.guild.channels.find('name', 'member-log');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
+  var joindate = Date.now()
+  logchannel.send("``` ${member} joined On: " + joindate + "```");
 });
 
 
 //!ping will reply pong 
-client.on('message', message => {
-  if (message.content === '!ping') {
-    message.reply('pong!');
+client.on('message', msg => {
+  if (msg.content === '!ping') {
+    msg.reply('pong!');
   }
 });
 
 //!avatar will reply a link to the users avatar
-client.on('message', message => {
-  if (message.content === '!avatar') {
-    message.reply(message.author.avatarURL);
+client.on('message', msg => {
+  if (msg.content === '!avatar') {
+    msg.reply(msg.author.avatarURL);
   }
 });
 
 //!test will reply if the bot is working
-client.on('message', message => {
-  if (message.content === '!test') {
-    message.reply('Emerald Is Working!');
+client.on('message', msg => {
+  if (msg.content === '!test') {
+   msg.reply('Emerald Is Online!');
+  }
+});
+
+//!github will reply to the user a link to the bot github page
+client.on("message", function(msg) {
+  if(msg.content === "!github"){
+    msg.reply("https://github.com/Vailyer/Emerald-Bot");
   }
 });
 
 
-client.login(token);
+
+
+client.login(config.token);
